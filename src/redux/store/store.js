5e6ae9a816eaa651d380fbc35e3a88cef50b41f5
jovid1242 <jovid1242@gmail.com>
2022-11-19 19:24:04 +0500

    fix card

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from '../auth/authSlice'
-import postSlice from '../post/postSlice'
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
-        posts: postSlice,
     },
 })
 
