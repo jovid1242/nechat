feca5f044759176d7f0a9d65c20eb88a835d235a
jovid1242 <jovid1242@gmail.com>
2022-11-19 16:28:27 +0500

    singup

---
+++ src/pages/SingUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../components/auth/SingUp'
+
+const SingUp = () => {
+    return (
+        <>
+            <Main />
+        </>
+    )
+}
+
+export default SingUp
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,32 +2,34 @@
 import Login from '../pages/auth/Login'
 import Home from '../pages/Home'
 import Profile from '../pages/Profile'
+import SingUp from '../pages/SingUp'
 
 export const routeNames = {
     HOME: '/',
     LOGIN: '/login',
     PROFILE: '/profile',
+    SINGUP: '/singup',
 }
 
 export const publicRoutes = [
     {
-        id: 1,
+        id: 2011,
         component: Home,
         path: routeNames.HOME,
     },
     {
-        id: 2,
+        id: 2012,
         component: Login,
         path: routeNames.LOGIN,
+    },
+    {
+        id: 2013,
+        component: SingUp,
+        path: routeNames.SINGUP,
     },
 ]
 
 export const privateRoutes = [
-    {
-        id: 1,
-        component: Home,
-        path: routeNames.HOME,
-    },
     {
         id: 2,
         component: Profile,
